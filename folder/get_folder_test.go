760adb2f359048dd739ca0024bce601b61a5b49f
curpix <183813905+curpix@users.noreply.github.com>
2024-10-05 22:03:24 +1000

    test: add test for get all folders

--- folder/get_folder_test.go
+++ folder/get_folder_test.go
@@ -5,8 +5,15 @@ import (
 
 	"github.com/georgechieng-sc/interns-2022/folder"
 	"github.com/gofrs/uuid"
-	// "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
+
+func Test_folder_GetAllFolders(t *testing.T) {
+	t.Parallel()
+
+	var result []folder.Folder = folder.GetAllFolders()
+	assert.NotNil(t, result, "Folders are returned")
+}
 
 // feel free to change how the unit test is structured
 func Test_folder_GetFoldersByOrgID(t *testing.T) {
