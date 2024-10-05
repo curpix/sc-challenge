13bb20c095227c39d3c1bd8c4a5ec678d8dccc44
curpix <183813905+curpix@users.noreply.github.com>
2024-10-05 23:14:35 +1000

    test: add test for get folders by orgid

--- folder/get_folder_test.go
+++ folder/get_folder_test.go
@@ -15,22 +15,61 @@ func Test_folder_GetAllFolders(t *testing.T) {
 	assert.NotNil(t, result, "Folders are returned")
 }
 
-// feel free to change how the unit test is structured
 func Test_folder_GetFoldersByOrgID(t *testing.T) {
 	t.Parallel()
+
+	var validOrgId = uuid.FromStringOrNil("c59cc5c1-9b81-4d00-95e3-22c6efdaf134")
+
 	tests := [...]struct {
-		name    string
-		orgID   uuid.UUID
-		folders []folder.Folder
-		want    []folder.Folder
+		testName string
+		orgID    uuid.UUID
+		folders  []folder.Folder
+		want     []folder.Folder
 	}{
-		// TODO: your tests here
+		{
+			testName: "No folders gives empty slice",
+			orgID:    validOrgId,
+			folders:  []folder.Folder{},
+			want:     []folder.Folder{},
+		},
+		{
+			testName: "orgID not present gives empty slice",
+			orgID:    validOrgId,
+			folders: []folder.Folder{
+				{
+					Name:  "fold",
+					OrgId: uuid.FromStringOrNil("5e35ff8f-21dd-4ed5-b861-ba93dbcdadc3"),
+					Paths: "fold",
+				},
+			},
+			want: []folder.Folder{},
+		},
+		{
+			testName: "orgID present in root folder",
+			orgID:    validOrgId,
+			folders: []folder.Folder{
+				{Name: "fold", OrgId: validOrgId, Paths: "fold"},
+			},
+			want: []folder.Folder{{Name: "fold", OrgId: validOrgId, Paths: "fold"}},
+		},
+		{
+			testName: "orgID present in parent and child folder - return both",
+			orgID:    validOrgId,
+			folders: []folder.Folder{
+				{Name: "fold", OrgId: validOrgId, Paths: "fold"},
+				{Name: "child-fold", OrgId: validOrgId, Paths: "fold.child-fold"},
+			},
+			want: []folder.Folder{
+				{Name: "fold", OrgId: validOrgId, Paths: "fold"},
+				{Name: "child-fold", OrgId: validOrgId, Paths: "fold.child-fold"},
+			},
+		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			// f := folder.NewDriver(tt.folders)
-			// get := f.GetFoldersByOrgID(tt.orgID)
-
+		t.Run(tt.testName, func(t *testing.T) {
+			f := folder.NewDriver(tt.folders)
+			folders := f.GetFoldersByOrgID(tt.orgID)
+			assert.Equal(t, tt.want, folders, tt.testName)
 		})
 	}
 }
