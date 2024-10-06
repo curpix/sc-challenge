c063c8693cf433ea7ae8dbce9ae9b2f7d1a4af6b
curpix <183813905+curpix@users.noreply.github.com>
2024-10-06 11:58:17 +1100

    docs: update docs for folder api with assumptions

--- folder/folder.go
+++ folder/folder.go
@@ -5,13 +5,14 @@ import "github.com/gofrs/uuid"
 type IDriver interface {
 	// GetFoldersByOrgID returns all folders that belong to a specific orgID.
 	GetFoldersByOrgID(orgID uuid.UUID) []Folder
+
 	// component 1
-	// Implement the following methods:
-	// GetAllChildFolders returns all child folders of a specific folder.
+	// GetAllChildFolders returns all child folders of a specific folder within same organisation.
+	// Duplicate files with same names but different paths are possible,
+	// where all possible children are returned without duplicates
 	GetAllChildFolders(orgID uuid.UUID, name string) ([]Folder, error)
 
 	// component 2
-	// Implement the following methods:
 	// MoveFolder moves a folder to a new destination.
 	// Assumption: Names are unique, as cannot distinguish between different paths.
 	// E.g. We have a, c.a and d. moveFolder("d", "a") is ambiguous, as we don't know
@@ -20,17 +21,11 @@ type IDriver interface {
 }
 
 type driver struct {
-	// define attributes here
-	// data structure to store folders
-	// or preprocessed data
-
-	// example: feel free to change the data structure, if slice is not what you want
 	folders []Folder
 }
 
 func NewDriver(folders []Folder) IDriver {
 	return &driver{
-		// initialize attributes here
 		folders: folders,
 	}
 }
